3f41385e7482b085b459d41100e0fd3caf4b813d
Ratul Nandy <ratul.tester@gmail.com>
2026-01-30 16:51:08 +0530

    newly added

--- src/test/java/com/restassuredrestfulbooker/CRUDoperations/C/EndToEndTesting/testFullEndToEndTC.java
+++ src/test/java/com/restassuredrestfulbooker/CRUDoperations/C/EndToEndTesting/testFullEndToEndTC.java
@@ -8,6 +8,11 @@ import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.Test;
 
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class testFullEndToEndTC {
     String Base_Url = "https://restful-booker.herokuapp.com";
     String Base_Path = "/booking";
@@ -43,7 +48,7 @@ public class testFullEndToEndTC {
             "    \"lastname\" : \"Brown\"\n" +
             "}";
     String token;
-    String bookingid;
+    Integer bookingid;
 
     RequestSpecification rs = RestAssured.given();
     Response r;
@@ -60,6 +65,71 @@ public class testFullEndToEndTC {
 
         vr = r.then().log().all().statusCode(200);
 
-        token = r.then().extract().path("token");
+        //token = r.then().extract().path("token");
+
+        String authResponse = r.asString();
+        JsonPath jsonPath = new JsonPath(authResponse);
+        token = jsonPath.getString("token");
+
+        assertThat(token).isNotNull().isAlphanumeric();
+    }
+
+    @Test
+    public void testBookingToGetBookingId (){
+        rs.baseUri(Base_Url);
+        rs.basePath(Base_Path);
+        rs.contentType(ContentType.JSON);
+        rs.body(Booking_Payload);
+
+        r = rs.when().log().all().post();
+
+        vr = r.then().log().all().statusCode(200);
+
+        //bookingid = r.then().extract().path("bookingid");
+        //System.out.println("BOOKING ID is :::: "+bookingid);
+
+        String bookingIdResponse = r.asString();
+        JsonPath jsonPath = new JsonPath(bookingIdResponse);
+        bookingid = jsonPath.getInt("bookingid");
+
+        assertThat(bookingid).isNotNull().isBetween(0,9999);
+    }
+
+    @Test
+    public void testCreatedBooking (){
+        rs.baseUri(Base_Url);
+        rs.basePath(Base_Path+"/"+bookingid);
+
+        r = rs.when().log().all().get();
+
+        vr = r.then().log().all().statusCode(200);
+
+        String tcbResponse = r.asString();
+        JsonPath jsonPath = new JsonPath(tcbResponse);
+
+        String tcbFirstName = jsonPath.getString("firstname");
+        assertThat(tcbFirstName).containsIgnoringCase("jIM");
+
+        String tcbLastName = jsonPath.getString("lastname");
+        assertThat(tcbLastName).containsIgnoringCase("Brown");
+
+        Integer tcbTotalPrice = jsonPath.getInt("totalprice");
+        assertThat(tcbTotalPrice).isNotNull().isNotNegative().isNotZero();
+
+        Boolean tcbDepositPaid = jsonPath.getBoolean("depositpaid");
+        assertThat(tcbDepositPaid).isTrue();
+
+        Object tcbbookingDates = jsonPath.getJsonObject("bookingdates");
+        assertThat(tcbbookingDates).isNotNull();
+
+        String tcbcheckin = jsonPath.getJsonObject("bookingdates.checkin");
+        assertThat(tcbcheckin).isEqualTo("2018-01-01");
+
+        String tcbcheckout = jsonPath.getJsonObject("bookingdates.checkout");
+        assertThat(tcbcheckout).isEqualTo("2019-01-01");
+
+        String tcbAdditionalNeeds = jsonPath.getString("additionalneeds");
+        assertThat(tcbAdditionalNeeds).containsIgnoringCase("breakfast");
+
     }
 }
