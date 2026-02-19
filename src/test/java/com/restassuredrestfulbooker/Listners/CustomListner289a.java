efb4f2912db2fed9f46eff10b2dd57acbf7c3915
Ratul Nandy <ratul.tester@gmail.com>
2026-02-19 16:32:22 +0530

    today

---
+++ src/test/java/com/restassuredrestfulbooker/Listners/CustomListner289a.java
@@ -0,0 +1,90 @@
+package com.restassuredrestfulbooker.Listners;
+
+import org.testng.*;
+
+public class CustomListner289a implements ISuiteListener, ITestListener, IClassListener, IInvokedMethodListener {
+    @Override
+    public void onBeforeClass(ITestClass testClass) {
+        IClassListener.super.onBeforeClass(testClass);
+    }
+
+    @Override
+    public void onAfterClass(ITestClass testClass) {
+        IClassListener.super.onAfterClass(testClass);
+    }
+
+    @Override
+    public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+        IInvokedMethodListener.super.beforeInvocation(method, testResult);
+    }
+
+    @Override
+    public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+        IInvokedMethodListener.super.afterInvocation(method, testResult);
+    }
+
+    @Override
+    public void beforeInvocation(IInvokedMethod method, ITestResult testResult, ITestContext context) {
+        IInvokedMethodListener.super.beforeInvocation(method, testResult, context);
+    }
+
+    @Override
+    public void afterInvocation(IInvokedMethod method, ITestResult testResult, ITestContext context) {
+        IInvokedMethodListener.super.afterInvocation(method, testResult, context);
+    }
+
+    @Override
+    public void onStart(ISuite suite) {
+        ISuiteListener.super.onStart(suite);
+    }
+
+    @Override
+    public void onFinish(ISuite suite) {
+        ISuiteListener.super.onFinish(suite);
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ITestListener.super.onTestStart(result);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        ITestListener.super.onTestSuccess(result);
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        ITestListener.super.onTestFailure(result);
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        ITestListener.super.onTestSkipped(result);
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
+    }
+
+    @Override
+    public void onTestFailedWithTimeout(ITestResult result) {
+        ITestListener.super.onTestFailedWithTimeout(result);
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        ITestListener.super.onFinish(context);
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        ITestListener.super.onStart(context);
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return ISuiteListener.super.isEnabled();
+    }
+}
--- src/test/java/com/restassuredrestfulbooker/Listners/Lab289a.java
+++ src/test/java/com/restassuredrestfulbooker/Listners/Lab289a.java
@@ -1,7 +1,52 @@
 package com.restassuredrestfulbooker.Listners;
 
+import org.testng.annotations.*;
+
+@Listeners(CustomListner289a.class)
 public class Lab289a {
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("I will be displayed Before-Suite");
+    }
+
+    @BeforeTest
+    public void beforeTest(){
+        System.out.println("I will be displayed Before-Test");
+    }
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("I will be displayed Before-Class");
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("I will be displayed Before-Method");
+    }
+
+    @Test
     public void steps(){
-        System.out.println("Suite Test Class Method");
+        System.out.println("This is the Main Test Case and Execution Part");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("I will be displayed After-Method");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("I will be displayed After-Class");
+    }
+
+    @AfterTest
+    public void afterTest(){
+        System.out.println("I will be displayed After-Test");
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("I will be displayed After-Suite");
     }
 }
